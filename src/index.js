2778dfa82b000d8382eb539f4ee5f004f256d2c0
Abhishek0090 <palabhishek411@gmail.com>
2021-10-06 15:16:26 +0530

    image with global attributes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import ReactDom from "react-dom";
 const name = "Abhishek Pal"
 const date = new Date().toLocaleDateString();
 const time = new Date().toLocaleTimeString();
+const img = "https://picsum.photos/300/3 00";
+const link = "https://gooogle.com"
 
 ReactDom.render(
   <>
-  <h1> Hello ,My name is {name}</h1>
-  <h2>today Date is {date}</h2>
-  <h3>today Date is {time}</h3>
+  <h1 contentEditable="true" > Hello ,My name is {name}</h1>
+  <img src={img} alt="img" />
+  <a href={link} target="_blank">
+    <img src={img} alt="random"/>
+  </a>
   </>,
   document.getElementById('root')
 );
