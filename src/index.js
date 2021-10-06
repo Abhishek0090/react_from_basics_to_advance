84eaade9b6962ed7dafd32cec6cf797234fac445
Abhishek0090 <palabhishek411@gmail.com>
2021-10-06 14:40:51 +0530

    es6 template

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const myname  = "Abhishek"
+const myrole = "Software developer"
 
 ReactDOM.render(
   <>
-   <h1>my name is {myname}</h1>
+   <h1>{`my name is ${myname} ${myrole}`}</h1>
    <p>my age is {10+10}</p>
-   <p>my age is {Math.random()}</p>
   </>,
   document.getElementById("root")
 );
