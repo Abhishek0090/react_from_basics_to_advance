171320cded071bda5ba47638fc8ffb1fd9a02efb
Abhishek0090 <palabhishek411@gmail.com>
2021-10-06 14:14:48 +0530

    simple movie list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const arr = ["Abhi","Hackur","Rectify","Slugify"]
-//In React, v16 its possible for render()
-//to return an array of elements.
-
 ReactDOM.render(
-  [
-    <h1>Abhishek Pal</h1>,
-    <h2>I m Trying something different</h2>,
-    <p>This is me</p>
-  ],
+  <>
+    <h1>Abhishek Pal</h1>
+    <hr/>
+    <h2>Favourite anime series</h2>
+    <ol>
+      <li>Death note</li>
+      <li>Attack on Titan</li>
+      <li>Jujutsu kaisen</li>
+      <li>Dragon ball</li>
+      <li>Naruto</li>
+    </ol>
+  </>,
   document.getElementById("root")
 );
