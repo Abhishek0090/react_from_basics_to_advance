12870dc7961bef851da661a93cee2bba3f63ec5d
Abhishek0090 <palabhishek411@gmail.com>
2021-10-06 15:39:26 +0530

    css setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './index.css';
 
 const name = "Abhishek Pal"
-const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
-const img = "https://picsum.photos/300/300";
+
+const img = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
 const link = "https://gooogle.com"
 
 ReactDom.render(
   <>
-  <h1 contentEditable="true" > Hello ,My name is {name}</h1>
+  <div className="container">
+  <h1 className="heading"> Hello ,My name is {name}</h1>
   <img src={img} alt="img" />
+  <img src={img2} alt="img" />
   <a href={link} target="_blank">
-    <img src={img} alt="random"/>
+    <img src={img3} alt="random"/>
   </a>
+  </div>
   </>,
   document.getElementById('root')
 );
