1b17c8e659ef8f61257b50ad391f322317a3f7c4
Abhishek0090 <palabhishek411@gmail.com>
2021-10-07 12:22:05 +0530

    components basics

---
+++ src/Para.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Para(){
+    return(
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, illo veritatis nisi inventore eaque exercitationem at ipsam voluptatum, reprehenderit odio aperiam officiis voluptatibus?</p>
+    );
+}
+
+export default Para;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-const name = "Abhishek Pal"
-
-const img = "https://picsum.photos/200/300";
-const img2 = "https://picsum.photos/250/300";
-const img3 = "https://picsum.photos/300/300";
-const link = "https://gooogle.com"
-
-ReactDom.render(
-  <>
-  <div className="container">
-  <h1 className="heading"> Hello ,My name is {name}</h1>
-  <img src={img} alt="img" />
-  <img src={img2} alt="img" />
-  <a href={link} target="_blank">
-    <img src={img3} alt="random"/>
-  </a>
-  </div>
-  </>,
-  document.getElementById('root')
-);
+ReactDom.render(<App />, document.getElementById("root"));
