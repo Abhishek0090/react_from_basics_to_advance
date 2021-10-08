e32a756c21ec74cccac064ef7961901c375e8962
Abhishek0090 <palabhishek411@gmail.com>
2021-10-08 14:09:07 +0530

    Es6 import and export

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import React from "react";
+const youtuber = "Abhishek";
 
-function App() {
-  //new Date(year, month , day , hours , minutes ,seconds , milliseconds)
-  let currDate = new Date();
-  currDate = currDate.getHours();
-  let greet = "";
-  const cssStyle = {};
 
-  if (currDate > 1 && currDate < 12) {
-    greet = "Good Morning";
-    cssStyle.color = "Green";
-  } else if (currDate >= 12 && currDate < 16) {
-    greet = "Good Afternoon";
-    cssStyle.color = "Orange";
-  } else if (currDate >= 17 && currDate < 6) {
-    greet = "Good Evening";
-    cssStyle.color = "Brown";
-  } else {
-    greet = "Good night";
-    cssStyle.color = "Black";
-  }
-  return (
-    <>
-      <div className="design">
-        <h1>
-          Hello Abhishek , <span style={cssStyle}>{greet}</span>
-        </h1>
-      </div>
-    </>
-  );
+const myname = "Hackurr";
+
+function myName(){
+    let namee = "Abhishek";
+    return namee;
 }
 
-export default App;
+export default youtuber;
+
+export {myname , myName};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App";
+import youtuber, {myname , myName} from "./App";
+import * as Abhi from "./App";
 import "./index.css";
 
-ReactDom.render(<App />, document.getElementById("root"));
+
+ReactDom.render(<><li>{Abhi.myName()}</li> <li>{myName()}</li> <h1>{myname}</h1></>, document.getElementById("root"));
