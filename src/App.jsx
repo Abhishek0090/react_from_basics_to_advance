7b740bb5b1b8208678050fef5dad1de0da65ef6e
Abhishek0090 <palabhishek411@gmail.com>
2021-10-14 17:25:43 +0530

    events base

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 
 
 const App = ()=>{
-    var currTime = new Date().toLocaleTimeString()
-    const [time, setTime] = useState(currTime)
-    setInterval(() => {
-     setTime(currTime)
-    }, 1000);
+    let color = "red"
+    const [bg, setBg] = useState(color)
+        const [name, setName] = useState("click  me 😏")
+    
+    const bgChange =()=>{
+        let newColor = "blue";
+        setBg(newColor)
+        setName("HAckurr 😍😍")
+    }
 
+    const bgdouble = ()=>{
+        let doub = "pink"
+        setBg(doub)
+        setName('Bruhh 😎😋😋')
+    }
   return (
     <>
-     <div className="div_style">
-     <h1 className="heading_style" style={{fontSize:"5rem"}}>{time}</h1>
+     <div className="div_style" style={{backgroundColor : bg}}>
+     <button onClick={bgChange} className="btn" onDoubleClick={bgdouble}>{name}</button>
      </div>
     </>
   );
