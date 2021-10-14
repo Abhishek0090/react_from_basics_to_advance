d47221a2cb3ed3343058c8fb2de6db81fc26b19d
Abhishek0090 <palabhishek411@gmail.com>
2021-10-14 20:42:48 +0530

    Login forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 
+const App = () => {
+  const [name, setName] = useState("hello");
+  const [Mname, setMname] = useState("");
+  const [Lname, setLname] = useState("");
+  const [last, setLast] = useState("");
 
-const App = ()=>{
-   
-    const [name, setName] = useState("hello")
-    const [Mname, setMname] = useState("")
+  const inputEvent = (e) => {
+    setName(e.target.value);
+  };
 
-    const inputEvent = (e)=>{
-        setName(e.target.value)
-    }
+  const inputLast = (e) => {
+    setLast(e.target.value);
+  };
 
-    const onSubmit = ()=>{
-        setMname(name)
-    }
+  const onSubmit = (e) => {
+    e.preventDefault(); //to prevent default reload after submitting
+    setMname(name);
+    setLname(last)
+  };
 
   return (
     <>
-    <div className="div_style">
-    <h1>{Mname}</h1>
-    <input type="text" name="name" id="name" placeholder="Enter Your Name" onChange={inputEvent} value={name} />
-    <button className="btn" onClick={onSubmit}>Submit 🤩</button>
-    </div>
+      <div>
+        <form action="" onSubmit={onSubmit}>
+          <div className="div_style">
+            <h1 style={{ textAlign: "center" }}>Hello {Mname} {Lname}</h1>
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              onChange={inputEvent}
+              value={name}
+            />
+            <input
+              type="text"
+              placeholder="Enter Your LastName"
+              onChange={inputLast}
+              value={last}
+            />
+            
+            <button type="submit" className="btn">
+              Submit 🤩
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
